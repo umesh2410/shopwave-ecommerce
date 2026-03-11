f1b666253c8471abbe2301534bde2dbca0c76b34
Umesh <makwanau2410@gmail.com>
2026-03-11 13:10:37 +0530

    Revert to standard pg driver as Render blocks Neon WebSockets

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,7 +1,8 @@
-const { Pool } = require('@neondatabase/serverless');
+const { Pool } = require('pg');
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
+  ssl: process.env.DATABASE_URL?.includes('sslmode=require') ? { rejectUnauthorized: false } : false,
 });
 
 pool.on('connect', () => {
